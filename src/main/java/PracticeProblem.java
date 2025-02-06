1ea88a4a47c41a60d2ba294b632dcb0a6fbf7d8d
karthikeya-arikirevula-ics4u1-02-25 <karthikeya.arikirevula@student.tdsb.on.ca>
2025-02-06 15:15:57 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -13,37 +13,78 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		Scanner in = new Scanner(System.in);
+		String question1;
+		System.out.print("In: ");
+		question1 = in.nextLine();
+		System.out.println(question1.charAt(0));
 		
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		Scanner in = new Scanner(System.in);
+		boolean question2;
+		System.out.print("In: ");
+		question2 = in.nextBoolean();
+		in.nextLine();
+		System.out.println(!question2);
 		
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		Scanner in = new Scanner(System.in);
+		int question3;
+		System.out.print("In: ");
+		question3 = in.nextInt();
+		in.nextLine();
+		System.out.println(question3 > 5);
 		
 	}
 
 	public static void q4() {
-		//Write question 4 code here
-		
+		Scanner in = new Scanner(System.in);
+		double question4;
+		System.out.print("In: ");
+		question4 = in.nextDouble();
+		in.nextLine();
+		System.out.println(question4 >= -2 && question4 <= 2);
+
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		Scanner in = new Scanner(System.in);
+		String question5;
+		System.out.print("In: ");
+		question5 = in.nextLine();
+		System.out.println(question5.equals("Hello World"));
 		
 	}
 
 	public static void q6() {
-		//Write question 6 code here
+		Scanner in = new Scanner(System.in);
+		int question6first;
+		System.out.print("In: ");
+		question6first = in.nextInt();
+		in.nextLine();
+		int question6second;
+		System.out.print("In: ");
+		question6second = in.nextInt();
+		in.nextLine();
+		System.out.println(question6first <= question6second);
 		
 	}
 
 	public static void q7() {
-		//Write question 7 code here
+		Scanner in = new Scanner(System.in);
+		double question7first;
+		System.out.print("In: ");
+		question7first = in.nextDouble();
+		in.nextLine();
+		double question7second;
+		System.out.print("In: ");
+		question7second = in.nextDouble();
+		in.nextLine();
+		System.out.println(question7first > question7second);
 		
 	}
 
